6488167e004f105ad825a38725d420544a46a1d5
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2025-04-14 16:57:28 +0300

    added a test for the frontend

---
+++ graphql_frontend/test/test.spec.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import Authors from "../src/components/Authors";
+import { ALLAUTHORS } from '../src/components/queries';
+
+const mocks = [
+  {
+    request: {
+      query: ALLAUTHORS,
+      variables: {}
+    },
+    result: {
+      data: {
+        allAuthors: [
+          {
+            "name": "Victor Hugo",
+            "booksN": 1,
+            "born": null,
+          },
+          {
+            "name": "Fyodor Dostoevsky",
+            "booksN": 3,
+            "born": 1800,
+          },
+        ]
+      }
+    }
+  }
+];
+
+it("renders without error", async () => {
+  render(
+    <MockedProvider mocks={mocks}>
+      <Authors show={true}/>
+    </MockedProvider>
+  );
+  expect(await screen.findByText("Fyodor Dostoevsky")).toBeInTheDocument();
+});
