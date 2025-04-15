ef58594c6330da4ead5b29384eeca1a63483ed76
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2025-04-15 10:48:38 +0300

    fixed server test

--- graphql_server/test/test.spec.js
+++ graphql_server/test/test.spec.js
@@ -1,35 +1,46 @@
 const { ApolloServer } = require("@apollo/server");
-const { expect, it } = require("@jest/globals");
+const { expect, it, beforeAll, afterAll, describe } = require("@jest/globals");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const typeDefs = require("../schema");
 const resolvers = require("../resolvers");
 
-it("test DB connection", async () => {
-  const MONGODB = process.env.MONGODB_URI;
+describe("server testing", () => {
+  // not modifying data in the DB bc we're not using a separate test DB
+  let testServer;
 
-  mongoose
-    .connect(MONGODB)
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-      console.log("error connection to MongoDB:", error.message);
+  beforeAll(() => {
+    const MONGODB = process.env.MONGODB_URI;
+    mongoose
+      .connect(MONGODB)
+      .then(() => {
+        console.log("Connected to MongoDB");
+      })
+      .catch((error) => {
+        console.log("error connection to MongoDB:", error.message);
+      });
+
+    testServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+  });
+
+  afterAll(async () => {
+    await testServer?.stop();
+    mongoose.connection.close();
+  });
+
+  it("test DB connection", async () => {
+    const response = await testServer.executeOperation({
+      query: "query AllAuthors { allAuthors { name } }",
     });
 
-  const testServer = new ApolloServer({
-    typeDefs,
-    resolvers,
+    expect(response.body.kind === "single");
+
+    expect(response.body.singleResult.errors).toBeUndefined();
+
+    expect(response.body.singleResult.data?.allAuthors[0]?.name).toBeTruthy();
   });
-
-  const response = await testServer.executeOperation({
-    query: "query Query { bookCount }",
-  });
-
-  expect(response.body.kind === "single");
-
-  // expect(response.body.singleResult.errors).toBeUndefined();
-
-  //expect(response.body.singleResult.data?.bookCount).toBeGreaterThanOrEqual(0);
 });
