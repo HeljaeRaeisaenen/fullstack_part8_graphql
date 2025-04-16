d88010fdbfac5c43bf6a58398f26f6a1c4a84a3a
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2025-04-16 13:18:18 +0300

    testing e2e tests

---
+++ tests-e2e/authors.test.js
@@ -0,0 +1,16 @@
+// @ts-check
+import { test, describe, expect, beforeEach } from "@playwright/test";
+
+describe("Library app", () => {
+  beforeEach(async ({ page }) => {
+    await page.goto("");
+  });
+  test("has heading", async ({ page }) => {
+    await expect(page.getByRole("heading", { name: "authors" })).toBeVisible();
+  });
+
+  test("books link", async ({ page }) => {
+    await page.getByRole("button", { name: "books" }).click();
+    await expect(page.getByText("published")).toBeVisible();
+  });
+});
